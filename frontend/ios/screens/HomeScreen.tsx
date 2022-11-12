24564837dcf3f84ea8f3414dcf2fcbbe67a749ae
pudderludde <ludvikb@online.no>
2022-11-12 14:44:02 +0100

    setup apollo query

--- frontend/ios/screens/HomeScreen.tsx
+++ frontend/ios/screens/HomeScreen.tsx
@@ -1,8 +1,46 @@
 import Header from '../components/Header';
-import { useQuery } from "@apollo/client";
+import { useQuery, gql } from "@apollo/client";
+import { useEffect } from 'react';
+import { Text } from "react-native";
+
+
+const GET_SEARCH = gql`
+query Get_Search ($searchWord: String, $skip: Int, $amount:Int, $year: Int, $order: Int){
+    songSearch(skip: $skip, amount:$amount, searchWord: $searchWord, year: $year, order: $order ) {
+        songID
+        artistName
+        songName
+        imageURL
+    }
+}
+`;
+
+type Song = {
+  songID: number,
+  songName: string,
+  artistName: string,
+  imageURL: string
+}
 
 export default function HomeScreen() {
+
+  const offset = 0
+  const pageSize = 4
+  const searchWord = ''
+  const year = 0
+  const order = -1
+
+  const {data} = useQuery(GET_SEARCH, {variables: { skip: offset, amount: pageSize, searchWord: searchWord, year: year, order: order }});
+
+
   return (
-    <Header/>
+    <>
+      <Header/>
+      {data?.songSearch.map((song: Song) => {
+        return (
+          <Text>{ song.songName }</Text>
+        )
+      })}
+    </>
   );
 }
