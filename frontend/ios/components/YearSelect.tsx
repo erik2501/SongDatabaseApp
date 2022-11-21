f20447c2d3b91b171fbe5bf4f4686e1aa81b0753
pudderludde <ludvikb@online.no>
2022-11-21 21:26:07 +0100

    made yearselect get years from database

--- frontend/ios/components/YearSelect.tsx
+++ frontend/ios/components/YearSelect.tsx
@@ -12,54 +12,14 @@ interface IProps {
 const YearSelect = ({year, setYear}: IProps) => {
 
   const {data} = useQuery(GET_DISTINCT_YEARS);
-    
- /*
-  //console.log(data.getDistinctYears)
 
-  const item = data.getDistinctYears.map((year : number) => (
-      {
-          label: year,
-          value: year,
-      }))
+  let items = [{ label: "All years", value: 0}]
 
-  console.log(item)
-
-  const range = (start : number, end : number) => Array.from(Array(end - start + 1).keys()).map(x => x + start);      
-
-  const years = range(1999,2020)
-
-              items={data.getDistinctYears.map((year : number, year1 : string) => (
-                  {
-                      label: year1,
-                      value: year,
-                  }))}
-
-          items= {item}
-*/    
-  const items = [        
-    { label: "All years", value: 0},            
-    { label: "1999", value: 1999 },
-    { label: "2000", value: 2000 },
-    { label: "2001", value: 2001 },
-    { label: "2002", value: 2002 },
-    { label: "2003", value: 2003 },
-    { label: "2004", value: 2004 },
-    { label: "2005", value: 2005 },
-    { label: "2006", value: 2006 },
-    { label: "2007", value: 2007 },
-    { label: "2008", value: 2008 },
-    { label: "2009", value: 2009 },
-    { label: "2010", value: 2010 },
-    { label: "2011", value: 2011 },
-    { label: "2012", value: 2012 },
-    { label: "2013", value: 2013 },                        
-    { label: "2014", value: 2014 },
-    { label: "2015", value: 2015 },                        
-    { label: "2016", value: 2016 },
-    { label: "2017", value: 2017 },
-    { label: "2018", value: 2018 },
-    { label: "2019", value: 2019 }
-  ]
+  if (data){
+    data.getDistinctYears.map((year : number) => {
+      items.push({label: year.toString(), value: year})
+    })
+  }
 
   return (
     <Dropdown
--- frontend/ios/screens/SongScreen.tsx
+++ frontend/ios/screens/SongScreen.tsx
@@ -1,7 +1,7 @@
 import { ScrollView, View, StyleSheet, Dimensions } from "react-native";
 import SongDetails from "../components/SongDetails";
 import { RootStackParamList } from "../helpers/types";
-import { NavigationProp, RouteProp } from "@react-navigation/native";
+import { RouteProp } from "@react-navigation/native";
 import ReviewComponent from "../components/ReviewComponent";
 import ListReviews from "../components/ListReviewComponent";
 import { useSetRecoilState } from 'recoil';
@@ -9,10 +9,9 @@ import { orientationAtom } from '../shared/globalState';
 
 interface SongScreenProps {
     route: RouteProp<RootStackParamList, "SongScreen">
-    navigation: NavigationProp<RootStackParamList, "SongScreen">
 }
 
-const SongScreen = ({ route, navigation }: SongScreenProps) => {
+const SongScreen = ({ route }: SongScreenProps) => {
 
     // Function that returns true if the screen is in portrait mode, and false if in landscape mode
     const isPortrait = () => {
